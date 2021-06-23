afe85fbbeca6a8f555910c33c05806c9bb61f4bb
masakiinoue <masaki.yagui@gmail.com>
2021-06-24 08:58:48 +0900

    initial commit

---
+++ components/sub/Hoge.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Sub Hoge</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <!-- components/Hoge.vue -->
+    <Hoge />
+    <!-- components/sub/Hoge.vue -->
+    <SubHoge />
+    <!-- components/sub/index.vue -->
+    <Sub />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
